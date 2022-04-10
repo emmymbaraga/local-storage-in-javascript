ac8fbac9781901fcd3d2125ca29eb29e10e99eab
emmymbaraga <emmnish92@gmail.com>
2022-04-10 09:25:10 +0200

    adding script and css files

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    const inpKey = document.getElementById("inpKey");
+    const inpValue = document.getElementById("inpValue");
+    const btnInsert = document.getElementById("btnInsert");
+    const IsOutput = document.getElementById("IsOutput");
+    
+    btnInsert.onclick = function(){
+        const key = inpKey.value;
+        const value = inpValue.value;
+        // console.log(key)
+        // console.log(value)
+
+        if(key&&value){
+            localStorage.setItem(key,value);
+            location.reload();
+        }
+    };
+
+    for(let i=0;i<localStorage.length;i++){
+        const key = localStorage.key(i);
+        const value=localStorage.getItem(key);
+
+        IsOutput.innerHTML += `${key}: ${value} <br/>`;
+    }
+   
